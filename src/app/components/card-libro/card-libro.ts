76693d4a5e848eb0191db013b9d18a1f74a06602
f-p-r <fperezre@gmail.com>
2025-10-23 14:58:55 +0200

    rc2

--- src/app/components/card-libro/card-libro.ts
+++ src/app/components/card-libro/card-libro.ts
@@ -1,12 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Libro } from '../../services/libros';
-import { CurrencyPipe } from '@angular/common';
 import { PrecioEuroPipe } from '../../pipes/precio-euro-pipe';
 
 @Component({
   selector: 'app-card-libro',
   standalone: true,
-  imports: [CurrencyPipe, PrecioEuroPipe],
+  imports: [PrecioEuroPipe],
   templateUrl: './card-libro.html',
   styleUrl: './card-libro.css'
 })
